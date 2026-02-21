cd31e055e1ba94cc368d2506d0ce2f2d0613f159
Osama Nabil <osama.nabeel.ajina@gmail.com>
2026-02-21 17:59:26 +0000

    ضع هذه الصورة https://drive.google.com/file/d/14cwnk23PZ4k3KdGTs-P5CIDOX

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'tahalib.inzox.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
         port: '',
         pathname: '/**',
       },
