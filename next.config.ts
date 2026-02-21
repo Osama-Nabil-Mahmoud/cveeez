eaa56dde2326f9e252a1b2257e9708cf1a20d6cb
Osama Nabil <osama.nabeel.ajina@gmail.com>
2026-02-21 18:04:02 +0000

    https://mir-s3-cdn-cf.behance.net/projects/404/368878213699271.Y3JvcCwxM

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'mir-s3-cdn-cf.behance.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
